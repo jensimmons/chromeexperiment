5b7b0660c93645481a39d61e907a85a5600f1eff
Jen Simmons <jen@jensimmons.com>
2013-04-26 17:41:17 -0400

    documenting the js

--- js/scripts.js
+++ js/scripts.js
@@ -1,60 +1,61 @@
 (function ($) {
   $(document).ready(function() {
-  
+
+// When the word "breathe" is clicked, show the navigation
     $('.stepone').on('click', function (evt) {
       evt.preventDefault();
       evt.stopPropagation();
       $(this).parents('body').addClass('nav-showing');
     });
 
+// When the small circle is clicked, show the navigation
     $('nav.nav-placeholder').on('click', function (evt) {
       evt.preventDefault();
       evt.stopPropagation();
       $(this).parents('body').removeClass('nav-collapsed').addClass('nav-showing');
     });
 
+// Any time any navigation icon is clicked, shrink the navigation into the corner & show the new panel
     $('nav a.breathing-link').on('click', function (evt) {
       evt.preventDefault();
       evt.stopPropagation();
       $(this).parents('body').removeClass().addClass('breathing-on nav-collapsed');
     });
-
     $('nav a.beingpresent-link').on('click', function (evt) {
       evt.preventDefault();
       evt.stopPropagation();
       $(this).parents('body').removeClass().addClass('beingpresent-on nav-collapsed');
     });
-
     $('nav a.mediating-link').on('click', function (evt) {
       evt.preventDefault();
       evt.stopPropagation();
       $(this).parents('body').removeClass().addClass('mediating-on nav-collapsed');
     });
-
     $('nav a.connecting-link').on('click', function (evt) {
       evt.preventDefault();
       evt.stopPropagation();
       $(this).parents('body').removeClass().addClass('connecting-on nav-collapsed');
     });
-
+    $('nav a.hiding-link').on('click', function (evt) {
+      evt.preventDefault();
+      evt.stopPropagation();
+      $(this).parents('body').removeClass().addClass('hiding-on nav-collapsed');
+    });
     $('nav a.disconnecting-link').on('click', function (evt) {
       evt.preventDefault();
       evt.stopPropagation();
       $(this).parents('body').removeClass().addClass('disconnecting-on nav-collapsed');
     });
-
     $('nav a.looking-link').on('click', function (evt) {
       evt.preventDefault();
       evt.stopPropagation();
       $(this).parents('body').removeClass().addClass('looking-on nav-collapsed');
     });
-
     $('nav a.feeling-link').on('click', function (evt) {
       evt.preventDefault();
       evt.stopPropagation();
       $(this).parents('body').removeClass().addClass('feeling-on nav-collapsed');
-    });
-    
+    });    
     
   });
 })(jQuery);
