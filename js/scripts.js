4ab1257117196f460cacb447348beb58e109805c
Jen Simmons <jen@jensimmons.com>
2013-04-26 17:22:04 -0400

    making nav toggle work again

--- js/scripts.js
+++ js/scripts.js
@@ -3,46 +3,55 @@
   
     $('.stepone').on('click', function (evt) {
       evt.preventDefault();
+      evt.stopPropagation();
       $(this).parents('body').addClass('nav-showing');
     });
 
     $('nav.nav-placeholder').on('click', function (evt) {
       evt.preventDefault();
+      evt.stopPropagation();
       $(this).parents('body').removeClass('nav-collapsed').addClass('nav-showing');
     });
 
     $('nav a.breathing-link').on('click', function (evt) {
       evt.preventDefault();
+      evt.stopPropagation();
       $(this).parents('body').removeClass().addClass('breathing-on nav-collapsed');
     });
 
     $('nav a.beingpresent-link').on('click', function (evt) {
       evt.preventDefault();
+      evt.stopPropagation();
       $(this).parents('body').removeClass().addClass('beingpresent-on nav-collapsed');
     });
 
     $('nav a.mediating-link').on('click', function (evt) {
       evt.preventDefault();
+      evt.stopPropagation();
       $(this).parents('body').removeClass().addClass('mediating-on nav-collapsed');
     });
 
-    $('nav a..connecting-link').on('click', function (evt) {
+    $('nav a.connecting-link').on('click', function (evt) {
       evt.preventDefault();
+      evt.stopPropagation();
       $(this).parents('body').removeClass().addClass('connecting-on nav-collapsed');
     });
 
     $('nav a.disconnecting-link').on('click', function (evt) {
       evt.preventDefault();
+      evt.stopPropagation();
       $(this).parents('body').removeClass().addClass('disconnecting-on nav-collapsed');
     });
 
     $('nav a.looking-link').on('click', function (evt) {
       evt.preventDefault();
+      evt.stopPropagation();
       $(this).parents('body').removeClass().addClass('looking-on nav-collapsed');
     });
 
     $('nav a.feeling-link').on('click', function (evt) {
       evt.preventDefault();
+      evt.stopPropagation();
       $(this).parents('body').removeClass().addClass('feeling-on nav-collapsed');
     });
     
